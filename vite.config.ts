9b2e25e4c77f1902561ad5b1213e076614adcdd9
R. Rajesh Jeba Anbiah <4907427+rrjanbiah@users.noreply.github.com>
2022-04-12 04:45:33 +0530

    Set base for GitHub pages hosting

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: './', // default is / and it breaks in GitHub pages  
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
